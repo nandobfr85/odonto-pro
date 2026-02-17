797a35552d0cb8ec7bcf82570903ff99efc3faac
Fernando Junior <fernandobfr10@gmail.com>
2026-02-17 02:24:06 -0300

    feat: create hero component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./_components/Header";
+import { Hero } from "./_components/Hero";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <div>
+        <Hero />
+      </div>
     </div>
   );
 }
