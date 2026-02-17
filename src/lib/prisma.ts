9dd250a86636ffb9371b170787eca30d6defad57
Fernando Junior <fernandobfr10@gmail.com>
2026-02-17 19:06:10 -0300

    feat: configure prisma and create models

---
+++ src/lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+let prisma: PrismaClient;
+const adapter = new PrismaPg({ connectionString });
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({ adapter });
+}
+
+let globalWithPrisma = global as typeof globalThis & { prisma: PrismaClient };
+
+if (!globalWithPrisma.prisma) {
+  globalWithPrisma.prisma = new PrismaClient({ adapter });
+}
+
+prisma = globalWithPrisma.prisma;
+
+export { prisma };
